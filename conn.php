2cfede350651aecccf887678e090cf7cfeae2d36
mikolin2000 <miguel.leal@softtek.com>
2015-12-10 20:43:18 -0600

    conn updated

--- conn.php
+++ conn.php
@@ -4,7 +4,7 @@ $db_config = parse_ini_file("/etc/dbapp.cfg");
 
 mysql_connect( $db_config['db_server'], $db_config['db_user'], $db_config['db_user_password'] )or
                 die("Could not connect: " . mysql_error());
-                mysql_select_db("db");
+                mysql_select_db("dbapp");
 
 ?>
 
