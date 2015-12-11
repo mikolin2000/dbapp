093d5b2863e6d81b8a3b7b4d1954560d2ffe5be4
mikolin2000 <miguel.leal@softtek.com>
2015-12-10 20:04:59 -0600

    conn.php updated

--- conn.php
+++ conn.php
@@ -1,6 +1,10 @@
 <?php
-mysql_connect('localhost', 'root','')or
-		die("Could not connect: " . mysql_error());
-		mysql_select_db("db");
-		
+
+$db_config = parse_ini_file("/etc/dbapp.cfg");
+
+mysql_connect( $db_config['db_server'], $db_config['db_user'], $db_config['db_user_password'] )or
+                die("Could not connect: " . mysql_error());
+                mysql_select_db("db");
+
 ?>
+
